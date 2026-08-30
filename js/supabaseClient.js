7ebc5f78d6118bc9220c43b2ad714dd322f1eb88
Min Khant Zin <leorio521530@gmail.com>
2026-08-31 02:40:03 +0630

    Update Supabase client URL and key

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -4,7 +4,7 @@
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const supabaseUrl = 'YOUR_PROJECT_URL'; // e.g. https://xxxxx.supabase.co
-const supabaseKey = 'YOUR_ANON_PUBLIC_KEY';
+const supabaseUrl = 'https://stlvhydxkyyawiwajvhn.supabase.co/';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0bHZoeWR4a3l5YXdpd2FqdmhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3OTYyMTcsImV4cCI6MjEwMzM3MjIxN30.kWXcY57j6M1kRFTpY6JBsJ5wPineXoQGWjqCHLpp8ds';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
